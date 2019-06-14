5c5e3f78334f6d8900aa765879dbcf42057160e3
Eric Tovar <tovareric@gmail.com>
2019-06-14 18:01:55 -0500

    setting up base.html

--- bugcollector/main_app/models.py
+++ bugcollector/main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.urls import reverse
 
+class Bug(models.Model):
+    name = models.CharField(max_length=100)
+    type_bug = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    age = models.IntegerField()
 # Create your models here.
--- bugcollector/main_app/views.py
+++ bugcollector/main_app/views.py
@@ -2,8 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+class Bug:
+    def __init__(self, name, type_bug, description, age):
+        self.name = name
+        self.type_bug = type_bug
+        self.description = description
+        self.age = age
+
+bugs = [
+    Bug('peter', 'spider', 'creepy crawly', 4),
+    Bug('Ralph', 'catapiller', 'chubby and green', 1+"month"),
+    Bug('doug', 'fly', 'will fly on your trash', 0),
+
+]
+
 def home(request):
     return HttpResponse('Bug Collecter Homepage')
 
 def about(request):
     return render(request, 'about.html')
+
+def bugs_index(request):
+    return render(request, 'bugs/index.html', {'bugs': bugs })
+
