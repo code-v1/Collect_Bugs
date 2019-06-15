8e486b6305d620f1dad2793d46d9854bc18860a1
Eric Tovar <tovareric@gmail.com>
2019-06-15 15:48:52 -0500

    setting up index and detail url

--- bugcollector/main_app/urls.py
+++ bugcollector/main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('cats/', views.cats_index, name='index'),
+    path('bugs/', views.bugs_index, name='index'),
+    path('bugs/<int:bug_id>/', views.bugs_detail, name='detail'),
 ]
